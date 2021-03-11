b7b491f244c1836b239af70eff64629e30030644
Imma-Olly <oluomajoan@gmail.com>
2021-03-11 10:20:09 -0500

    added to animation function

--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -5,3 +5,17 @@ export function getMergeSortAnimations(array) {
     mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
     return animations;
   }
+
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+  }
